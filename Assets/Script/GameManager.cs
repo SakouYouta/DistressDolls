d0d11e681404bb93c04315373b40a58af0ee6701
tamago-108 <genki.1081004@gmail.com>
2024-11-18 17:00:08 +0900

    ターン終了機能の追加

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -2,19 +2,88 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class GameManager : MonoBehaviour
 {
     [SerializeField] CardController cardPrefab;
-    [SerializeField] Transform playerHand,enemyHand, playerField, enemyField;
+    [SerializeField] Transform playerHand, playerField, enemyField;
+
+    bool isPlayerTurn = true; //
+    List<int> deck = new List<int>() { 1, 2, 3, 1, 1, 2, 2, 3, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3 };  //
 
     void Start()
     {
         StartGame();
     }
 
-    void StartGame()
+    void StartGame() // 初期値の設定 
     {
-        CardController card = Instantiate(cardPrefab, playerHand);
-        card.Init(1);
+        // 初期手札を配る
+        SetStartHand();
+
+        // ターンの決定
+        TurnCalc();
+    }
+
+    void CreateCard(int cardID, Transform place)
+    {
+        CardController card = Instantiate(cardPrefab, place);
+        card.Init(cardID);
+    }
+
+    void DrawCard(Transform hand) // カードを引く
+    {
+        // デッキがないなら引かない
+        if (deck.Count == 0)
+        {
+            return;
+        }
+
+        // デッキの一番上のカードを抜き取り、手札に加える
+        int cardID = deck[0];
+        deck.RemoveAt(0);
+        CreateCard(cardID, hand);
+    }
+
+    void SetStartHand() // 手札を3枚配る
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            DrawCard(playerHand);
+        }
+    }
+
+    void TurnCalc() // ターンを管理する
+    {
+        if (isPlayerTurn)
+        {
+            PlayerTurn();
+        }
+        else
+        {
+            EnemyTurn();
+        }
+    }
+
+    public void ChangeTurn() // ターンエンドボタンにつける処理
+    {
+        isPlayerTurn = !isPlayerTurn; // ターンを逆にする
+        TurnCalc(); // ターンを相手に回す
+    }
+
+    void PlayerTurn()
+    {
+        Debug.Log("Playerのターン");
+
+        DrawCard(playerHand); // 手札を一枚加える
+    }
+
+    void EnemyTurn()
+    {
+        Debug.Log("Enemyのターン");
+
+        CreateCard(1, enemyField); // カードを召喚
+
+        ChangeTurn(); // ターンエンドする
     }
 }
