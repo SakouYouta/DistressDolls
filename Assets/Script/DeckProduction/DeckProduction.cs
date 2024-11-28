db26f7d4aa44d6f3363c6205f916a26656c91602
SakouYouta <sakouyouta0322@icloud.com>
2024-11-28 12:24:43 +0900

    11/28(木)の進捗コミット

--- Assets/Script/DeckProduction/DeckProduction.cs
+++ Assets/Script/DeckProduction/DeckProduction.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 
 public class DeckProduction : MonoBehaviour
 {
+    [SerializeField] private MyCardView cardView;
+    List<int> DebugCardID = new List<int> { 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53 };
     List<int> deck1 = new List<int>();// デッキ1を格納するリスト
 
     void Start()
     {
-        
+        cardView.DisplayCards(DebugCardID);
     }
 
     // Update is called once per frame
---
+++ Assets/Script/DeckProduction/MyCardView.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+using Debug = UnityEngine.Debug;
+
+public class MyCardView : MonoBehaviour
+{
+    [SerializeField] CardController cardPrefab;
+    [SerializeField] private Transform panel; // Panel
+
+    public void DisplayCards(List<int> cardIds)
+    {
+        foreach (int cardID in cardIds)
+        {
+            CardController card = Instantiate(cardPrefab, panel);
+            card.Init(cardID);
+        }
+    }
+}
