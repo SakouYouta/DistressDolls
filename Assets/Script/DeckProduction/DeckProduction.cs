0fefe0197cb4571236f548011723c53ca951e490
SakouYouta <sakouyouta0322@icloud.com>
2024-11-28 09:46:05 +0900

    ブランチを保存するためのコミット

---
+++ Assets/Script/DeckProduction/DeckProduction.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeckProduction : MonoBehaviour
+{
+    List<int> deck1 = new List<int>();// デッキ1を格納するリスト
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
