49e972a598cfd191c61cd61016b3a22e46c89919
SakouYouta <sakouyouta0322@icloud.com>
2024-12-02 19:00:30 +0900

    12/02（月）デッキ作成機能の追加、α完成？

--- Assets/Script/DeckProduction/DeckProduction.cs
+++ Assets/Script/DeckProduction/DeckProduction.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 public class DeckProduction : MonoBehaviour
 {
     [SerializeField] private MyCardView cardView;
+    [SerializeField] private SetDeck setDeck;
     List<int> DebugCardID = new List<int> { 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53 };// 仮のリスト
+    List<int> DeckRegister = new List<int>();// デッキの保存用リスト
 
     void Start()
     {
-        cardView.DisplayCards(DebugCardID);
+        cardView.DisplayCards(DebugCardID);    
     }
 
     void Update()
     {
 
+    }
+
+    // デッキを保存する関数
+    public void SaveDeck()
+    {
+        DeckRegister = setDeck.GetDeck();
+        Debug.Log("デッキリスト：" + string.Join(", ", DeckRegister));
+        JsonSaveManager.SaveDeckList(DeckRegister);
+    }
+
+    public void ReadDeck()
+    {
+        JsonSaveManager.LoadDeckList();
     }
 }
--- Assets/Script/DeckProduction/SetDeck.cs
+++ Assets/Script/DeckProduction/SetDeck.cs
@@ -8,21 +8,18 @@ public class SetDeck : MonoBehaviour, IDropHandler
 
     public void OnDrop(PointerEventData eventData)
     {
-        // ドラッグしているオブジェクトから CardMovement を取得
         CardMovement cardMove = eventData.pointerDrag.GetComponent<CardMovement>();
         if (cardMove != null)
         {
-            // ドロップされたカードの CardController を取得
             CardController cardController = cardMove.GetComponent<CardController>();
             if (cardController != null)
             {
-                // カードのモデル情報を取得
                 CardModel cardModel = cardController.model;
                 if (cardModel != null)
                 {
-                    // カードIDを取得
+                    // カードIDを登録
                     deck.Add(cardModel.cardId);
-                    Debug.Log("デッキリスト：" + string.Join(", ", deck));
+                    //Debug.Log("デッキリスト：" + string.Join(", ", deck));
                 }
                 else
                     Debug.LogError("カードモデル情報の取得に失敗しました");
@@ -33,5 +30,10 @@ public class SetDeck : MonoBehaviour, IDropHandler
         }
         else
             Debug.LogError("カードムーブ情報の取得に失敗しました");
+    }
+
+    public List<int> GetDeck()
+    {
+        return new List<int>(deck);
     }
 }
