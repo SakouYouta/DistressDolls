e8ebe5f51b700d79a136e59d58aa0ef4f14d9cb4
tamago-108 <genki.1081004@gmail.com>
2025-01-23 11:18:33 +0900

    パック開封でのカード選択機能の追加

--- Assets/Script/GachaSystem/GachaSystemManager.cs
+++ Assets/Script/GachaSystem/GachaSystemManager.cs
@@ -1,12 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 public class GachaSystemManager : MonoBehaviour
 {
-    [SerializeField] CardController cardPrefab; // カードプレハブ 
+    [SerializeField] CardController cardPrefab; // カードプレハブ
+    [SerializeField] Button pack1Button;       // Pack 1 ボタン
+    [SerializeField] Button pack2Button;       // Pack 2 ボタン
+    [SerializeField] Button pack3Button;       // Pack 3 ボタン
 
     // シングルトン化
     public static GachaSystemManager instance;
+
+    // 各パックのカードリスト
+    private List<int> packCardList1 = new List<int>() { 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23 };
+    private List<int> packCardList2 = new List<int>() { 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36 };
+    private List<int> packCardList3 = new List<int>() { 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49 };
+
+    private List<int> selectedPackList = null; // 選択されたパックのリスト
 
     private void Awake()
     {
@@ -21,11 +33,80 @@ public class GachaSystemManager : MonoBehaviour
         }
     }
 
-    // カードを生成するメソッド（Public化した）
+    private void Start()
+    {
+        // 各パック選択ボタンにリスナーを追加
+        pack1Button.onClick.AddListener(() => SelectPack(1));
+        pack2Button.onClick.AddListener(() => SelectPack(2));
+        pack3Button.onClick.AddListener(() => SelectPack(3));
+    }
+
+    // パックを選択するメソッド
+    public void SelectPack(int packNumber)
+    {
+        switch (packNumber)
+        {
+            case 1:
+                selectedPackList = packCardList1;
+                break;
+            case 2:
+                selectedPackList = packCardList2;
+                break;
+            case 3:
+                selectedPackList = packCardList3;
+                break;
+            default:
+                Debug.LogError("Invalid pack number");
+                return;
+        }
+
+        Debug.Log($"Pack {packNumber} selected.");
+
+        // 選択ボタンを非表示にし、開封ボタンを表示
+        pack1Button.gameObject.SetActive(false);
+        pack2Button.gameObject.SetActive(false);
+        pack3Button.gameObject.SetActive(false);
+
+        // OpenPackスクリプトに通知して開封ボタンを有効化
+        OpenPack openPack = FindObjectOfType<OpenPack>();
+        if (openPack != null)
+        {
+            openPack.EnableOpenButton();
+        }
+        else
+        {
+            Debug.LogError("OpenPack script not found!");
+        }
+    }
+
+    // 現在の選択パックリストを取得する
+    public List<int> GetSelectedPackList()
+    {
+        return selectedPackList;
+    }
+
+    // カードを生成するメソッド（OpenPackで利用される）
     public void OpenCardCreate(int cardId, Transform trans)
     {
-        // cardPrefabをopenedCardTransに生成する
+        if (selectedPackList == null)
+        {
+            Debug.LogError("No pack selected. Cannot create cards.");
+            return;
+        }
+
         CardController card = Instantiate(cardPrefab, trans);
         card.Init(cardId);
+    }
+
+    // カードIDをランダムに決めるメソッド（OpenPackが利用）
+    public int GetRandomCardId()
+    {
+        if (selectedPackList == null)
+        {
+            Debug.LogError("No pack selected. Cannot decide card ID.");
+            return -1;
+        }
+
+        return selectedPackList[Random.Range(0, selectedPackList.Count)];
     }
 }
--- Assets/Script/GachaSystem/OpenPack.cs
+++ Assets/Script/GachaSystem/OpenPack.cs
@@ -1,44 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OpenPack : MonoBehaviour
 {
-    [SerializeField] GameObject cardPackPrefab; // カードパックプレハブ
     [SerializeField] Transform openedCardTrans; // 開封したカードの生成場所
-    [SerializeField] Transform cardPackTrans; // カードパックの生成場所
+    [SerializeField] Button openButton;         // 開封ボタン
 
-    public List<int> packCardList1 = new List<int>() {1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }; // パックのカードリスト
+    private bool isPackOpened = false;  // パックが開封されたかどうかのフラグ
 
     private void Start()
     {
-        OpenPacks();
+        // 開封ボタンを非表示にする
+        openButton.gameObject.SetActive(false);
+    }
+
+    // 開封ボタンを有効化するメソッド
+    public void EnableOpenButton()
+    {
+        // ボタンを有効化して、リスナーを追加
+        openButton.gameObject.SetActive(true);
+
+        // ボタンが再表示されるたびにリスナーを1回だけ追加
+        if (!isPackOpened)
+        {
+            openButton.onClick.AddListener(OpenPacks);
+        }
     }
 
     // パックを開封するメソッド
     public void OpenPacks()
     {
+        if (isPackOpened) return;  // すでに開封済みの場合は処理しない
+
+        // GachaSystemManagerから選択されたカードリストを取得
+        List<int> selectedPackList = GachaSystemManager.instance.GetSelectedPackList();
+
+        if (selectedPackList == null || selectedPackList.Count == 0)
+        {
+            Debug.LogError("No pack selected or pack is empty.");
+            return;
+        }
 
         string displayText = "Opened Cards: "; // 表示用文字列を初期化
 
         // 3枚カードを生成する
         for (int i = 0; i < 3; i++)
         {
-            int cardId = decisionCardId();
-            GachaSystemManager.instance.OpenCardCreate(cardId, openedCardTrans);
+            int cardId = DecisionCardId(selectedPackList); // リストに基づいてランダムにカードIDを取得
+            GachaSystemManager.instance.OpenCardCreate(cardId, openedCardTrans); // カード生成
 
-            // 取得したカードIDを文字列に追加
+            // ログ用文字列を更新
             displayText += cardId + (i < 2 ? ", " : "");
         }
 
-        // デバッグログに表示
-        Debug.Log(displayText);
+        Debug.Log(displayText); // 開封結果をログに表示
+
+        // 開封後、ボタンを無効化して再度押せないようにする
+        openButton.gameObject.SetActive(false);
+
+        // 開封フラグを立てて、再度開封できないようにする
+        isPackOpened = true;
     }
 
-    // カードのIDをランダムに決めるメソッド
-    int decisionCardId()
+    // 選択されたリストからランダムにカードIDを決定するメソッド
+    private int DecisionCardId(List<int> cardList)
     {
-        int randomCardId = packCardList1[Random.Range(0, packCardList1.Count)];
-        return randomCardId;
+        return cardList[Random.Range(0, cardList.Count)];
     }
 }
+
