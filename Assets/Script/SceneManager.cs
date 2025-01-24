e9eee31c239c9c894beb6e94de4bfc6c7a71b6e3
tamago-108 <genki.1081004@gmail.com>
2025-01-24 12:02:35 +0900

    タイトルとメニュー画面を追加、カードIDの不測の追加も

---
+++ Assets/Script/SceneManager.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; // シーン切り替えを扱うための名前空間
+
+public class SceneManagerScript : MonoBehaviour
+{
+    // メニューシーンやバトルシーンなどの名前を定数として管理します
+    private const string TITLE_SCENE = "TitleScene";
+    private const string MENU_SCENE = "MenuScene";
+    private const string BATTLE_SCENE = "GameScene";
+    private const string GACHA_SCENE = "gachaSystem";
+    private const string DECK_SCENE = "DeckProduction";
+
+    /// <summary>
+    /// 指定されたシーンをロードします。
+    /// </summary>
+    /// <param name="sceneName">ロードしたいシーン名</param>
+    public void LoadScene(string sceneName)
+    {
+        // シーン名が正しいか確認しながら、指定されたシーンをロードします
+        if (SceneUtility.GetBuildIndexByScenePath(sceneName) != -1)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+        else
+        {
+            Debug.LogError("シーン名が無効です: " + sceneName);
+        }
+    }
+
+    /// <summary>
+    /// タイトルシーンに移動します。
+    /// </summary>
+    public void LoadTitleScene()
+    {
+        LoadScene(TITLE_SCENE);
+    }
+
+    /// <summary>
+    /// メニューシーンに移動します。
+    /// </summary>
+    public void LoadMenuScene()
+    {
+        LoadScene(MENU_SCENE);
+    }
+
+    /// <summary>
+    /// バトルシーンに移動します。
+    /// </summary>
+    public void LoadBattleScene()
+    {
+        LoadScene(BATTLE_SCENE);
+    }
+
+    /// <summary>
+    /// ガチャシーンに移動します。
+    /// </summary>
+    public void LoadGachaScene()
+    {
+        LoadScene(GACHA_SCENE);
+    }
+
+    /// <summary>
+    /// デッキ生成シーンに移動します。
+    /// </summary>
+    public void LoadDeckScene()
+    {
+        LoadScene(DECK_SCENE);
+    }
+}
