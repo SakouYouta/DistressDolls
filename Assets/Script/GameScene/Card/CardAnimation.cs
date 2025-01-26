7f5079251ea2d8d341401629bbe16dc35ae9886b
SakouYouta <sakouyouta0322@icloud.com>
2025-01-27 03:20:34 +0900

    アニメーション関数を実装

--- Assets/Script/GameScene/Card/CardAnimation.cs
+++ Assets/Script/GameScene/Card/CardAnimation.cs
@@ -1,10 +1,28 @@
+using System.Collections;
 using UnityEngine;
 
 public class CardAnimation : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private float rotationDuration = 0.75f; // 回転にかかる時間（秒）
+
+    #region RotateCard-カードを回転
+    public IEnumerator RotateCard(Transform card)
     {
-        
+        float elapsedTime = 0.0f; // 経過時間を記録する変数
+        float startRotation = card.eulerAngles.z;
+        float endRotation = startRotation + 360f; // 一周（360度）後の回転角度
+
+        while (elapsedTime < rotationDuration)
+        {
+            // 現在の回転角度を線形補間で計算
+            float currentRotation = Mathf.Lerp(startRotation, endRotation, elapsedTime / rotationDuration);
+            card.eulerAngles = new Vector3(0, 0, currentRotation);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        // 回転が終了した後、最終的に初期の向きにリセット（0度の状態に戻す）
+        card.eulerAngles = new Vector3(0, 0, startRotation);
     }
+    #endregion
 }
