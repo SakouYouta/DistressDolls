627f94270f5405979761c26f8068e81e335de15d
SakouYouta <sakouyouta0322@icloud.com>
2025-02-21 11:22:14 +0900

    リーダー設定機能の追加

--- Assets/Script/DeckProduction/SelectLeader.cs
+++ Assets/Script/DeckProduction/SelectLeader.cs
@@ -7,18 +7,21 @@ using System.Collections.Generic;
 
 public class SelectLeader : MonoBehaviour
 {
-    [SerializeField] private Image[] leaderImage;
+    [SerializeField] private Sprite[] leaderImage;
+    [SerializeField] private Button changeButton;
+    [SerializeField] private GameObject selectPanel;
     [SerializeField] private Button button1;
     [SerializeField] private Button button2;
     [SerializeField] private Button button3;
-    private int leader;
+    [SerializeField] private SetDeck setDeck;
 
     private void Start()
     {
-        leader = DataSaveManager.LoadDeckList().FirstOrDefault();
+        int leader = DataSaveManager.LoadDeckList().FirstOrDefault();
+        ChangeLeader(leader);
         button1.onClick.AddListener(() => ChangeLeader(1));
         button2.onClick.AddListener(() => ChangeLeader(2));
-        button2.onClick.AddListener(() => ChangeLeader(3));
+        button3.onClick.AddListener(() => ChangeLeader(3));
     }
 
     #region ChangeLeader()-リーダーの切り替え
@@ -30,18 +33,29 @@ public class SelectLeader : MonoBehaviour
             case 1 :
                 if (deck[0] != 1)
                     deck[0] = 1;
+                changeButton.image.sprite = leaderImage[0];
                 break;
             case 2 :
                 if (deck[0] != 2)
                     deck[0] = 2;
+                changeButton.image.sprite = leaderImage[1];
                 break;
             case 3 :
                 if (deck[0] != 3)
                     deck[0] = 3;
+                changeButton.image.sprite = leaderImage[2];
                 break;
             default : break;
         }
-        DataSaveManager.SaveDeckList(deck);
+        setDeck.deck = deck;
+        selectPanel.SetActive(false);
+    }
+    #endregion
+
+    #region ActivePanel()-セレクトパネルのアクティブ化
+    public void ActivePanel()
+    {
+        selectPanel.SetActive(true);
     }
     #endregion
 }
